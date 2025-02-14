54e6f5aba5f680e76a596868cc5764d1f73219ef
Raspi <raspipro@gmail.com>
2025-02-14 11:00:38 +0100

    ADD Pilote class

---
+++ Pilote/function/Pilote.py
@@ -0,0 +1,29 @@
+class Pilote:
+    speed = 0.0
+    direction = 0.0
+
+    def __init__(self, speed, direction): #Ajout de speed et direction au "tableau" de self
+        self.speed = speed
+        self.direction = direction
+
+    def adjustSpeed(self):
+        self.speed = 1.2 #ajuste la valeur de la vitesses
+        print(f"Ajustement de speed a : {self.speed}") #print la vitesse après actualisation
+        return self.speed #retourne la nouvelle valeur de vitesse 
+
+    def changeDirection(self):
+        self.direction = 2.1 #ajuste la direction
+        print(f"Ajustement de direction a : {self.direction}") #print la direction après ajustement
+        return self.direction #retourne la direction actualiser
+    
+    def applyBrakes(self):
+        print("Déclanchement des frein") #affiche le bon déclanchement des frein
+        return True #True pour oui le frein est déclancher
+    
+    def getCurrentSpeed(self):
+        print(f"Valeur actuelle de speed : {self.speed}") #affiche directement la valeur de speed
+        return self.speed #retourne la valeur de speed au besoins
+    
+    def getCurrentDirection(self):
+        print(f"Valeur actuelle de la direction prise : {self.direction}") # affiche directement la valeur de la direction
+        return self.direction # retourne la valeur de speed
---
+++ Pilote/main.py
@@ -0,0 +1,17 @@
+from function.Pilote import *
+
+def main():
+    pilote = Pilote(0.0, 0.0) #defini les valeur par defaut a speed 0 et direction 0 les deux en type float
+    
+    pilote.changeDirection()
+    # currentDirection = pilote.changeDirection()
+    # print(f"Direction actuelle : {currentDirection}")
+    pilote.adjustSpeed()
+    # currentSpeed = pilote.adjustSpeed()
+    # print(f"Speed actuelle : {currentSpeed}")
+    pilote.getCurrentDirection()
+    pilote.getCurrentSpeed()
+    pilote.applyBrakes()
+
+if __name__ == '__main__':
+    main()
