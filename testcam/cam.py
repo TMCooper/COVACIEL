67542be0199e5e201e992d9546b1839190f9f338
Raspi <raspipro@gmail.com>
2025-02-14 17:02:00 +0100

    update cam

--- testcam/cam.py
+++ testcam/cam.py
@@ -1,17 +1,3 @@
-from picamera2 import Picamera2
 from picamera2.outputs import FfmpegOutput
 
-# Créer une instance de la caméra
-picam2 = Picamera2()
-
-
-output = FfmpegOutput("-f mpegts udp://192.168.20.165:5000")  
-
-
-picam2.start_recording(output)
-
-
-picam2.wait_recording(3600)  
-
-
-picam2.stop_recording()
+output = FfmpegOutput("cam.mp4", audio=True)
