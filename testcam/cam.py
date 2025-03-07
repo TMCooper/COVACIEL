a7da52ab112ae21fc5478a7e2b105137657c642f
Raspi <raspipro@gmail.com>
2025-03-07 09:46:27 +0100

    Update testcam folder

--- testcam/cam.py
+++
@@ -1,7 +0,0 @@
-from picamera2.outputs import FileOutput
-import socket
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    
-sock.connect(("REMOTEIP", 10001))
-stream = sock.makefile("wb")
-output = FileOutput(stream)
